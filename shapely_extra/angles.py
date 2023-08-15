469bf2cbf00a125c549c9aebf02f95c147bc8856
Shawn <shawntaylor@weecology.org>
2023-08-15 06:51:34 -0600

    initial angles code w/ setup.py

---
+++ shapely_extra/angles.py
@@ -0,0 +1,339 @@
+from math import (
+    radians, degrees, sin, cos, atan2, pi)
+
+from shapely.geometry import Point, LineString
+
+# Thse are a few generalized angle methods designed to
+# work with shapely Point and Linestring
+
+# because pi is infinate there will always be some rounding errors.
+# Setting some precision ensures that, for example, sin(pi) = 0
+DECIMAL_PRECISION = 8
+
+def radiansToDegrees(radians):
+    """
+    Converts from radians to degrees.
+
+    Parameters
+    ----------
+    radians : float
+        An angle in radians.
+
+    Returns
+    -------
+    float
+        An angle in degrees
+
+    """
+    return degrees(radians)
+
+def degreesToRadians(angleDegrees):
+    """
+    Converts from degrees to radians.
+
+    Parameters
+    ----------
+    angleDegrees : float
+        An angle in degrees.
+
+    Returns
+    -------
+    float
+        An angle in radians.
+
+    """
+    return radians(angleDegrees)
+
+def angleBetweenPoints(p0, p1):
+    """
+    The angle of the vector from Points p0 to p1, relative to the positive x-axis.
+
+    Parameters
+    ----------
+    p0 : Point or tuple
+        A shapely Point object or (x,y) tuple
+    p1 : Point or tuple
+        A shapely Point object or (x,y) tuple
+
+    Returns
+    -------
+    float
+        The angle, in radians, is normalized to be in the range [ -Pi, Pi ].
+
+    """
+    if isinstance(p0, Point) and isinstance(p1, Point):
+        dx = p1.x - p0.x
+        dy = p1.y - p0.y
+    elif isinstance(p0, tuple) and isinstance(p1, tuple):
+        dx = p1[0] - p0[0]
+        dy = p1[1] - p0[1]
+    else:
+        raise TypeError('p0 and p1 must be shapely.geometry.Point or (x,y) tuples')
+
+    return atan2(dy,dx)
+
+def angleDiff(angle1, angle2):
+    """
+    The unoriented smallest difference between two angles.
+
+    Parameters
+    ----------
+    angle1 : float
+        Angle in radians in the range (-Pi,Pi)
+    angle2 : float
+        Angle in radians in the range (-Pi,Pi)
+
+    Returns
+    -------
+    float
+        Angle in radians in the range (0,Pi)
+
+    """
+    if angle1 < angle2:
+        angle_diff = angle2 - angle1
+    else:
+        angle_diff = angle1 - angle2
+        
+    if angle_diff > pi:
+        angle_diff = (2*pi) - angle_diff
+    
+    return round(angle_diff, DECIMAL_PRECISION)
+
+def angleBetweenVectors(start_point, middle_point, end_point):
+    """
+    Calculate the smallest angle of two joined vectors.
+    
+    This assumes the two vectors share a point at `middle_point`,
+    so can be described by the three points start, center, and end. 
+    
+    Parameters
+    ----------
+    start_point : Point or tuple
+        A shapely Point object or (x,y) tuple
+    middle_point : Point or tuple
+        A shapely Point object or (x,y) tuple
+    end_point : Point or tuple
+        A shapely Point object or (x,y) tuple
+
+    Returns
+    -------
+    float
+        Angle in radians in the range (0,Pi)
+
+    """
+    angle1 = angleBetweenPoints(middle_point, start_point)
+    angle2 = angleBetweenPoints(middle_point, end_point)
+    
+    return angleDiff(angle1, angle2)
+
+def pointFromAngleAndDistance(ref_point, angle, distance, use_radians=False):
+    """
+    Generate a new point with the specified angle and distance from ref_point.
+    
+    Angle is relative to the positive x-axis and should be in the 
+    range (-pi,pi) for radians and (-180,180) for degrees.
+
+    Parameters
+    ----------
+    ref_point : Point or tuple
+        A shapely point object or (x,y) tuple
+    angle : float
+        Angle, relative to the positive x-axis, to make the new point. Either
+        degrees or radians, specified by use_radians.
+    distance : float
+        Distance from ref_point for new point.
+    use_radians : bool, optional
+        Whether angle is radians. The default is False.
+
+    Returns
+    -------
+    point
+        shapely.geometry.Point
+
+    """
+    if isinstance(ref_point, Point):
+        x1, y1 = ref_point.x, ref_point.y
+    elif isinstance(ref_point, tuple):
+        x1, y1 = ref_point
+    else:
+        raise TypeError('ref_point must be shapely.geometry.Point or (x,y) tuple')
+
+    if not use_radians:
+        angle = degreesToRadians(angle)
+        
+    x_off = cos(angle) * distance
+    y_off = sin(angle) * distance
+    
+    x_off = round(x_off, DECIMAL_PRECISION)
+    y_off = round(y_off, DECIMAL_PRECISION)
+    
+    return Point((x1 + x_off, y1 + y_off))
+    
+    
+def perpendicularLineAtEndpoint(ref_line, length, location='end', attached = 'center'):
+    """
+    Generate a new line segment which is perpendicular to a reference line.
+
+    ref_line must have at least 1 segment. If more than 1 segment the perpendicular
+    angle is calculated based on `location`.
+    If location is 'end' then the new line will be perpendicular to the final
+    segment of ref_line.
+    If location is 'start' then the new line will be perpenducilar to the first
+    segment of ref_line.
+    
+    The new line will intersect with either the start or end of the
+    ref_line, depending on `location`.
+    If `attached=center` the new line will have the ref_line endpoint as
+    it's center.
+    If `attached` is right or left, then the new line will have either its 
+    right or left endpoints on the ref_line endpoint. Where, if the new line
+    is centered, 'left' is the point counter-clockwise from the ref_line, and 
+    'right' is the endpoint clockwise from ref_line.  
+
+    Parameters
+    ----------
+    ref_line : LineString
+        Shapely line with at least 2 points.
+    length : float
+        Length of the new line.
+    location : str
+        Either 'start' or 'end' for which line segment ref_line to calculate
+        the 90 degree angle from.
+
+    Returns
+    -------
+    new_line : LineString
+        shapely LineString
+
+    """
+    if not isinstance(ref_line, LineString):
+        raise TypeError('ref_line must be shapely LineString')
+        
+    # New perpendicular line will intersect at coord2
+    if location == 'end':
+        coord1 = ref_line.coords[-2]
+        coord2 = ref_line.coords[-1] 
+    elif location == 'start':
+        coord1 = ref_line.coords[1]
+        coord2 = ref_line.coords[0]
+    else:
+        raise ValueError('location must be start or end')
+    
+    if attached == 'center':
+        first_point_distance = length/2
+        second_point_distance = length/2
+    elif attached == 'left':
+        first_point_distance = 0
+        second_point_distance = length
+    elif attached == 'right':
+        first_point_distance = length
+        second_point_distance = 0
+    else:
+        raise ValueError('attached must be left,right, or center')
+    
+    ref_line_angle = angleBetweenPoints(coord1, coord2) # in radians
+    
+    first_point = pointFromAngleAndDistance(
+        ref_point = coord2,
+        angle = ref_line_angle + (pi/2),
+        distance = first_point_distance,
+        use_radians=True
+        )
+    second_point = pointFromAngleAndDistance(
+        ref_point = coord2,
+        angle = ref_line_angle - (pi/2),
+        distance = second_point_distance,
+        use_radians=True
+        )
+
+    return LineString([first_point, second_point])
+    
+
+def perpendicularLineAtMidpoint(ref_line, length, location='end', attached = 'center'):
+    """
+    Generate a new line segment which is perpendicular to a reference line with 3 points.
+
+    Given a ref_line of 2 segments, this will generate a new line which
+    intersects the midpoint at an angle which is the average of the 2 adjacent
+    segment angles. 
+
+    ref_line must have at least 2 segment. If more than 2 segment the perpendicular
+    angle is calculated based on `location`.
+    If location is 'end' then the new line will be perpendicular to the final 2
+    segments of ref_line.
+    If location is 'start' then the new line will be perpenducilar to the first 2
+    segment of ref_line.
+    
+    The new line will intersect with either the start or end of the
+    ref_line, depending on `location`.
+    If `attached=center` the new line will have the ref_line endpoint as
+    it's center.
+    If `attached` is right or left, then the new line will have either its 
+    right or left endpoints on the ref_line endpoint. Where, if the new line
+    is centered, 'left' is the point counter-clockwise from the ref_line, and 
+    'right' is the endpoint clockwise from ref_line.  
+
+    Parameters
+    ----------
+    ref_line : LineString
+        Shapely line with at least 2 segments (e.g. >= 3 coordinates).
+    length : float
+        Length of the new line.
+    location : str
+        Either 'start' or 'end' for which line segment ref_line to calculate
+        the 90 degree angle from.
+
+    Returns
+    -------
+    new_line : LineString
+        shapely LineString
+
+    """
+    if not isinstance(ref_line, LineString):
+        raise TypeError('ref_line must be shapely LineString')
+    if len(ref_line.coords) <= 2:
+        raise ValueError('ref_line must have at least 2 segments')
+    
+    # New perpendicular line will intersect at coord2
+    # coord3 is the line endpoint or startpoint
+    if location == 'end':
+        coord1 = ref_line.coords[-3]
+        coord2 = ref_line.coords[-2] 
+        coord3 = ref_line.coords[-1]
+    elif location == 'start':
+        coord1 = ref_line.coords[2]
+        coord2 = ref_line.coords[1]
+        coord3 = ref_line.coords[0]
+    else:
+        raise ValueError('location must be start or end')
+    
+    if attached == 'center':
+        first_point_distance = length/2
+        second_point_distance = length/2
+    elif attached == 'left':
+        first_point_distance = 0
+        second_point_distance = length
+    elif attached == 'right':
+        first_point_distance = length
+        second_point_distance = 0
+    else:
+        raise ValueError('attached must be left,right, or center')
+    
+    ref_line_angle1 = angleBetweenPoints(coord1, coord2) # in radians
+    ref_line_angle2 = angleBetweenPoints(coord2, coord3)
+    ref_line_angle_avg = (ref_line_angle1 + ref_line_angle2) /2
+    
+    first_point = pointFromAngleAndDistance(
+        ref_point = coord2,
+        angle = ref_line_angle_avg + (pi/2),
+        distance = first_point_distance,
+        use_radians=True
+        )
+    second_point = pointFromAngleAndDistance(
+        ref_point = coord2,
+        angle = ref_line_angle_avg - (pi/2),
+        distance = second_point_distance,
+        use_radians=True
+        )
+
+    return LineString([first_point, second_point])
---
+++ test/test_angles.py
@@ -0,0 +1,83 @@
+from shapely_extra.angles import (
+        radiansToDegrees,
+        degreesToRadians,
+        angleBetweenPoints,
+        angleDiff,
+        angleBetweenVectors,
+        pointFromAngleAndDistance,
+        perpendicularLineAtEndpoint,
+        )
+
+from shapely.geometry import Point, LineString
+from random import choices
+from math import isclose
+
+
+def test_rad_to_deg():
+    degrees = list(range(-180,180,1))
+    radians = [degreesToRadians(d) for d in degrees]
+    match = [isclose(d,radiansToDegrees(r)) for d, r in zip(degrees,radians)]
+    assert all(match)
+
+def test_angleBetweenPoints_input():
+    """A shapely point and tuple should have the same results"""
+    ints = range(0,1000)
+    k=500
+    points1 = [Point(x,y) for x,y in zip(choices(ints,k=k),choices(ints,k=k))]
+    points2 = [Point(x,y) for x,y in zip(choices(ints,k=k),choices(ints,k=k))]
+
+    matches = [angleBetweenPoints(p1,p2) == angleBetweenPoints((p1.x,p1.y),(p2.x,p2.y)) for p1, p2 in zip(points1,points2)]
+    print(matches)
+    assert all(matches)
+
+def test_angleBetweenPoints():
+    """Input angle for newly generated point should match resulting measured angle"""
+    angles = list(range(-179,181,1))
+    angles.extend([a/10 for a in angles]) # add some float angles as well
+    point = Point(0,0)
+    distance = 10
+    new_points = [pointFromAngleAndDistance(point, angle=a, distance=distance, use_radians=False) for a in angles]
+    new_angles = [angleBetweenPoints(point, new_p) for new_p in new_points]
+    new_angles = [radiansToDegrees(a) for a in new_angles]
+    matches = [isclose(angle, new_angle, rel_tol=1e-5) for angle,new_angle in zip(angles, new_angles)]
+
+    assert all(matches)
+
+def test_angleBetweenPoints_around_circle():
+    """Points placed equal angles around a circle should have the same distance"""
+    dist = 5
+    center = Point(0,0)
+    right  = pointFromAngleAndDistance(center, angle=0, distance=dist, use_radians=False)
+    top    = pointFromAngleAndDistance(center, angle=90, distance=dist, use_radians=False)
+    left   = pointFromAngleAndDistance(center, angle=180, distance=dist, use_radians=False)
+    bottom = pointFromAngleAndDistance(center, angle=-90, distance=dist, use_radians=False)
+
+    top_matches = isclose(top.distance(left), top.distance(right))
+    bottom_matches = isclose(bottom.distance(left), bottom.distance(right))
+    left_matches = isclose(left.distance(top), left.distance(bottom))
+    right_matches = isclose(right.distance(top), right.distance(bottom))
+
+    assert all([top_matches, bottom_matches, left_matches, right_matches])
+    
+def test_perpendicular_angle():
+    """Angle of new perpendicular line should be 90"""
+    line = LineString([(0,0),(5,10)])
+    new_line = perpendicularLineAtEndpoint(line, length=10, location='end', attached='center')
+
+    # New line consists of 2 points. both of which should be 90 deg. relative to endpoint.
+    new_angle1 = angleBetweenVectors(line.coords[0], line.coords[1], new_line.coords[0])
+    new_angle2 = angleBetweenVectors(line.coords[0], line.coords[1], new_line.coords[1])
+    new_angle1 = radiansToDegrees(new_angle1)
+    new_angle2 = radiansToDegrees(new_angle2)
+    # Rounding errors will happen, but this should hit 90 degrees within a pretty small tolerance
+    assert all([isclose(new_angle1,90, rel_tol=1e-5), isclose(new_angle2, 90.,rel_tol=1e-5)])
+
+def test_perpendicular_length():
+    """Length of new perpendicular line should match input length"""
+    lengths = list(range(1,20,1))
+    lengths.extend([l/10 for l in lengths])
+    line = LineString([(0,0),(5,10)])
+    new_lines = [perpendicularLineAtEndpoint(line, length=l, location='end', attached='center') for l in lengths]
+    lines_match = [isclose(length,new_line.length,rel_tol=1e-5) for length, new_line in zip(lengths, new_lines)]
+
+    assert all(lines_match)
